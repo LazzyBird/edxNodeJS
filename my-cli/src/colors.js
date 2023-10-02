6af5836188c44adf8291457fe0e29c24821c973d
LazzyBird <olgathek@gmail.com>
2023-10-02 15:20:32 +0300

    styling CLI

---
+++ my-cli/src/colors.js
@@ -0,0 +1,44 @@
+import chalk from "chalk";
+// Background colors
+export const bgBlue = chalk.bgRgb(52, 158, 219);
+export const bgCyan = chalk.bgRgb(26, 188, 156);
+export const bgGreen = chalk.bgRgb(46, 204, 113);
+export const bgPurple = chalk.bgRgb(142, 68, 173);
+export const bgRed = chalk.bgRgb(231, 76, 60);
+export const bgWhite = chalk.bgRgb(236, 240, 241);
+export const bgYellow = chalk.bgRgb(241, 196, 15);
+
+// Text colors
+export const txBlue = chalk.rgb(52, 152, 219);
+export const txCyan = chalk.rgb(26, 188, 156);
+export const txGreen = chalk.rgb(46, 204, 113);
+export const txPurple = chalk.rgb(142, 68, 173);
+export const txRed = chalk.rgb(231, 76, 60);
+export const txWhite = chalk.rgb(236, 240, 241);
+export const txYellow = chalk.rgb(241, 196, 15);
+
+// console.log(txYellow('Hello, I’m a Yellow string'))
+
+// Get the current timestamp
+const timestamp = () => new Date().toLocaleString();
+
+export const displayTimestamp = () => bgPurple(timestamp());
+
+export const displayInfo = (msg) => bgYellow.bold(`ℹ️ ${msg ?? "Info:"}`);
+
+export const displaySuccess = (msg = "") =>
+  bgCyan.inverse.bold(`✅ Success! ${msg}`);
+
+export const displayCategory = (category) => txGreen.bold.underline(category);
+
+export const displayAmount = (amount) => txYellow.bold.underline(`$${amount}`);
+
+export const displayID = (id) => txCyan.bold(id);
+
+export const displayName = (name) => txCyan(name);
+
+export const displayRRP = (rrp) => txYellow.bold(`$${rrp}`);
+
+export const displayText = (msg) => txPurple(msg);
+
+export const displayKey = (key) => txBlue.bold(key);
--- my-cli/src/utils.js
+++ my-cli/src/utils.js
@@ -5,14 +5,27 @@ const API = "http://localhost:3000";
 // Set the categories
 const categories = ["confectionery", "electronics"];
 
+import {
+  bgCyan,
+  bgPurple,
+  bgRed,
+  bgYellow,
+  txBlue,
+  txCyan,
+  txGreen,
+  txPurple,
+  txRed,
+  txYellow,
+} from "./colors.js";
+
+// Export the output display functions
+
 // Log the usage of the command to the console
-export const log = (msg) => {
-  console.log(`\n${msg}\n`);
-};
+export const log = (msg) => console.log(`\n${msg}\n`);
+
 // Log the error to the console
-export const error = (msg) => {
-  console.error(`\n${msg}\n`);
-};
+export const error = (msg) =>
+console.error(`${bgRed.inverse(⚠️ Error:)}\n${txRed(msg)}\n`);
 
 // Update the order with the given ID
 export async function update(id, amount) {
