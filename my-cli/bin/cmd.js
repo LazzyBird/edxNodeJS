dbdf46f5606109db2924dd1e924a8afdf924b809
LazzyBird <olgathek@gmail.com>
2023-10-02 13:00:19 +0300

    building CLI tools 15 of 21

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -1,72 +1,78 @@
 #!/usr/bin/env node
-import { got } from "got";
+// import { got } from "got"; // commented on 13 of 21 working with CLI tools
 import { Command } from "commander";
+import { update } from "../src/utils.js"
 // Create a new Command Program
 const program = new Command();
 const API = "http://localhost:3000";
 
 // Log the usage of the command to the console
-const usage = (msg = "Back office for My App") => {
+export const log = (msg) => {
     console.log(`\n${msg}\n`);
-  };
+};
+// Log the error to the console
+export const error = (msg) => {
+  console.error(`\n${msg}\n`);
+};
+// Update the order with the given ID
+async function updateItem(id, amount) {
+  usage(`Updating order ${id} with amount ${amount}`);
+  try {
+    if (isNaN(+amount)) {
+      usage("Error: <AMOUNT> must be a number");
+      process.exit(1);
+    }
+    // Use GOT to make a POST request to the API
+    await got.post(`${API}/orders/${id}`, {
+      json: { amount: +amount },
+    });
+    // Log the result to the console
+    usage(`Order ${id} updated with amount ${amount}`);
+  } catch (err) {
+    // If there is an error, log it to the console and exit
+    console.error(err.message);
+    process.exit(1);
+  }
+}
 
 // Create a new Program
 program
   .name("my-cli") // Set the name of the program
   .description("Back office for My App") // Set the description
   .version("1.0.0"); // Set the version
-// Parse the arguments from process.argv
-program.parse();
 
-// Get the arguments from the command line
-const argv = process.argv.slice(2);
-// If there are no arguments, show the usage and exit
-if (argv.length < 2) {
-  usage();
-  process.exit(1);
-}
-
-// Deconstruct the arguments into variables
-const [argID, argAmount] = argv;
-
-// Check if the Amount is a number
-const amount = parseInt(argAmount);
-// If the amount is not a number, show the usage and exit
-if (isNaN(amount)) {
-  usage("Error: <AMOUNT> must be a number");
-  process.exit(1);
-}
-// Update the order with the given ID
-async function updateItem(id,amount) {
-    usage(`Updating order ${id} with amount ${amount}`)
-    try {
-      if (isNaN(+amount)) {
-        usage("Error: <AMOUNT> must be a number");
-        process.exit(1);
-      }
-      // Use GOT to make a POST request to the API
-      await got.post(`${API}/orders/${id}`, {
-        json: { amount: +amount },
-      });
-      // Log the result to the console
-      usage(`Order ${id} updated with amount ${amount}`);
-    } catch (err) {
-      // If there is an error, log it to the console and exit
-      console.error(err.message);
-      process.exit(1);
-    }
-  };
-  
 // Create a command for adding a new order
 program
-// Set the command name
-.command("update")
-// Set the argument ID to be required
-.argument("<ID>", "Order ID")
-// Set the argument AMOUNT to be required
-.argument("<AMOUNT>", "Order Amount")
-// Set the action to be executed when the command is run
-.action(async (id,amount) => await updateItem(id,amount));
+  // Set the command name
+  .command("update")
+  // Set the argument ID to be required
+  .argument("<ID>", "Order ID")
+  // Set the argument AMOUNT to be required
+  .argument("<AMOUNT>", "Order Amount")
+  // Set the action to be executed when the command is run
+  .action(async (id, amount) => await updateItem(id, amount));
 
 // Parse the arguments from process.argv
 program.parse();
+
+// Create a command for listing categories by IDs
+program
+  // Set the command name
+  .command("add")
+  // Set the command description
+  .description("Add Product by ID to a Category")
+  // Set the category to be required
+  .argument("<CATEGORY>", "Product Category")
+  // Set the argument ID to be required
+  .argument("<ID>", "Product ID")
+  // Set the argument NAME to be required
+  .argument("<NAME>", "Product Name")
+  // Set the argument AMOUNT to be required
+  .argument("<AMOUNT>", "Product RRP")
+  // Set the argument INFO to be optional
+  .argument("[INFO...]", "Product Info")
+  // Set the action to be executed when the command is run
+  .action(
+    async (category, id, name, amount, info) =>
+      await add(category, id, name, amount, info)
+  );
